a19b0f2efc82d7ec8c357b78f986edf7a06421d9
tamiris-rtl <146030970+tamiris-rtl@users.noreply.github.com>
2023-11-05 12:04:30 -0300

    teste

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ let app = angular.module("App", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl: "template/home.html"
+            templateUrl: "./template/home.html"
         })
         .when("/estrategia", {
-            templateUrl: "template/estrategia.html"
+            templateUrl: "./template/estrategia.html"
         })
         .when("/tecnologia", {
-            templateUrl: "template/tecnologia.html"
+            templateUrl: "./template/tecnologia.html"
         })
         .when("/diversos", {
-            templateUrl: "template/diversos.html"
+            templateUrl: "./template/diversos.html"
         })
 
         .otherwise({redirectTo: "/"})
