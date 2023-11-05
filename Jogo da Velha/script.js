cff86e691ef0a3304e062fc533f2055c379ca4f7
Isadora Vieira <isavieiracamara@gmail.com>
2023-11-05 13:48:07 -0300

    Add files via upload

--- Jogo da Velha/script.js
+++ Jogo da Velha/script.js
@@ -1,9 +1,10 @@
-const currentPlayer =  document.querySelector(".currentPlayer");
+// Obtém uma referência para o elemento HTML com a classe "currentPlayer".
+const currentPlayer = document.querySelector(".currentPlayer");
 
-let selected;
-let player = "X";
-
-let positions = [
+// Declara variáveis globais
+let selected;  // Array que armazena os movimentos selecionados no jogo.
+let player = "X";  // Representa o jogador atual.
+let positions = [  // Matriz que contém todas as combinações vencedoras possíveis.
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
@@ -14,53 +15,64 @@ let positions = [
     [3, 5, 7],
 ];
 
-function init(){
-    selected = []
+// Função de inicialização do jogo
+function init() {
+    selected = [];  // Inicializa o array de movimentos selecionados.
 
+    // Exibe uma mensagem indicando qual jogador está na vez.
     currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`;
 
+    // Adiciona um evento de clique a todos os botões do jogo para permitir que os jogadores façam seus movimentos.
     document.querySelectorAll(".game button").forEach((item) => {
-        item.innerHTML = "";
-        item.addEventListener("click", newMove);
+        item.innerHTML = "";  // Limpa o conteúdo dos botões.
+        item.addEventListener("click", newMove);  // Adiciona um evento de clique para o novo movimento.
     });
 }
 
-init();
+init();  // Chama a função de inicialização do jogo quando a página é carregada.
 
-function newMove(e){
-    const index = e.target.getAttribute("data-i");
-    e.target.innerHTML = player;
-    e.target.removeEventListener("click", newMove);
-    selected[index] = player;
+// Função chamada quando um botão do jogo é clicado.
+function newMove(e) {
+    const index = e.target.getAttribute("data-i");  // Obtém o índice do botão clicado.
+    e.target.innerHTML = player;  // Define o conteúdo do botão como o símbolo do jogador atual.
+    e.target.removeEventListener("click", newMove);  // Remove a capacidade de clicar no botão novamente.
+    selected[index] = player;  // Registra o movimento no array.
 
+    // Aguarda um curto período de tempo antes de verificar o resultado do jogo.
     setTimeout(() => {
         check();
     }, [100]);
 
+    // Alterna para o próximo jogador e atualiza a mensagem exibida.
     player = player === "X" ? "O" : "X";
     currentPlayer.innerHTML = `JOGADOR DA VEZ: ${player}`;
 }
 
-function check(){
+// Função que verifica o resultado do jogo.
+function check() {
+    // Determina o jogador anterior com base no jogador atual.
     let playerLastMove = player === "X" ? "O" : "X";
 
+    // Filtra os movimentos do jogador anterior e obtém os índices deles.
     const items = selected
-        .map((item,i) => [item, i]) 
+        .map((item, i) => [item, i])
         .filter((item) => item[0] === playerLastMove)
         .map((item) => item[1]);
 
-    for(pos of positions){
-        if(pos.every((item) => items.includes(item))){
-            alert("O JOGADOR '" + playerLastMove + "' GANHOU!");
-            init();
+    // Itera sobre todas as combinações vencedoras possíveis.
+    for (pos of positions) {
+        // Verifica se o jogador anterior preencheu todas as posições em alguma combinação vencedora.
+        if (pos.every((item) => items.includes(item))) {
+            alert("O JOGADOR '" + playerLastMove + "' GANHOU!");  // Exibe um alerta com o jogador vencedor.
+            init();  // Reinicializa o jogo.
             return;
         }
     }
 
-    if(selected.filter((item) => item).length === 9){
-        alert('DEU EMPATE!');
-        init();
+    // Verifica se houve um empate (todos os espaços preenchidos) e exibe um alerta.
+    if (selected.filter((item) => item).length === 9) {
+        alert("DEU EMPATE!");
+        init();  // Reinicializa o jogo.
         return;
     }
-    
 }
